77a9e140c331b41208c665e19834cf8015dfd2bc
FarhamB.dev <165949272+FdevB@users.noreply.github.com>
2025-08-03 13:37:48 +0430

    Create MessageModel for message box in contact-us page

--- info_app/models.py
+++ info_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class MessageModel(models.Model):
+    """
+    Model definition for MessageModel.
+
+    This class defines the MessageModel database schema and its related behaviors.
+
+    Attributes:
+        name (CharField): The name of the message sender.
+        email (EmailField): The email address of the sender.
+        subject (CharField): The subject ot title of the message.
+        message (TextField): The message.
+        sent_at (DateTimeField): Timestamp of when the message was sent.
+        is_read (BooleanField): Indicates whether the message has been read.
+    """
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+    
+    is_read = models.BooleanField(default=False)
+
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Message'
+
+    def __str__(self):
+        return self.title
